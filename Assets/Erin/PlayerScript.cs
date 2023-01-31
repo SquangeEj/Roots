b977efb5b7aaeef4e96fcae531f4ba84686a2e13
Squange <78355315+SquangeEj@users.noreply.github.com>
2023-01-31 16:40:06 +0000

    Added timer

--- Assets/Erin/PlayerScript.cs
+++ Assets/Erin/PlayerScript.cs
@@ -1,21 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private float Speed = 5;
-    
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Slider Sldr;
+    private float stamina = 6f;
+    private Vector2 UserInput;
+
+    [SerializeField] private float Timer = 600;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 UserInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Timer -= 1 * Time.deltaTime;
+        
+        UserInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+    }
 
-        transform.Translate(UserInput * Speed * Time.deltaTime);
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && stamina > 0)
+        {
+            stamina -= 1 * Time.deltaTime;
+            Speed = 12;
+        }
+        else
+        {
+            if (stamina < 6f)
+            {
+                stamina += 1 * (Time.deltaTime * (Timer/1200)) ;
+            }
+            Speed = 6;
+        }
+        Debug.Log(stamina);
+        rb.MovePosition(rb.position + UserInput * (Speed * Time.deltaTime) * Timer/600);
     }
 }
